d6b031463e8e76b7a52cd45e956bab78a6983aa8
Anirudh Pundir <anirudhpundir007@gmail.com>
2024-09-14 13:42:45 +0530

    fetch section content added

--- src/controllers/adminControllers/section.controllers.js
+++ src/controllers/adminControllers/section.controllers.js
@@ -1,85 +1,121 @@
+import mongoose from "mongoose";
 import { Course } from "../../models/course.models.js";
 import { Section } from "../../models/section.models.js";
 import { ApiResponse } from "../../utils/apiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
-const addSection = asyncHandler(async(req, res) => {
-    
-    const {courseId, sectionName, sectionDetails} = req.body;
 
-    if(!courseId) return res.status(200).json(new ApiResponse(400, {success : false}, "Invalid Course Id"));
+const addSection = asyncHandler(async (req, res) => {
 
-    const course = await Course.findById(courseId);
+    const { courseId, sectionName, sectionDetails } = req.body;
 
-    if(!(course && !course.isDeleted)) return res.status(200).json(new ApiResponse(400, {success: false}, "Course not found"));
-
-    if(!sectionName || !sectionDetails) return res.status(200).json(new ApiResponse(400, {success: false}, "Invalid input"));
-
-    const sectionExits = await Section.find({courseId, isDeleted: false, sectionName});
-
-    if(sectionExits && sectionExits.length > 0) return res.status(200).json(new ApiResponse(400, {success: false}, 'Section already exists'));    
-
-    const section = await Section.create({courseId, sectionName, sectionDetails});
-
-    if(!section) return res.status(200).json(new ApiResponse(400, {success: false}, "Section was not created"));
-
-    return res.status(200).json(new ApiResponse(200, {success: true, data : section}, "Section was created successfully"));
-});
-
-const updateSection = asyncHandler(async(req, res) => {
-
-    const {sectionId, ...rest} = req.body;
-
-    if(!sectionId) return res.status(200).json(new ApiResponse(400, {success: false}, 'Section Id is missing'));
-
-    if(!rest) return res.status(200).json(new ApiResponse(400, {success: false}, "Invalid input"));
-
-    const section = await Section.findById(sectionId);
-
-    if(!(section && !section.isDeleted)) return res.status(200).json(new ApiResponse(404, {success: false}, "Section was not found"));
-
-    const updateSection = await Section.findByIdAndUpdate(sectionId, {...rest}, {new: true});
-
-    if(!updateSection) return res.status(200).json(new ApiResponse(400, {success: false}, "Section was not updated successfully"));
-
-    return res.status(200).json(new ApiResponse(200, {success: true, data : section}, "Section was updated successfully"));
-});
-
-const deleteSection = asyncHandler(async(req, res) => {
-
-    const {sectionId} = req.body;
-
-    if(!sectionId) return res.status(200).json(new ApiResponse(400, {success: false}))
-
-    const section = await Section.findById(sectionId);
-
-    if(!(section && !section.isDeleted)) return res.status(200).json(new ApiResponse(404, {success: false}, "Section was not found"));
-
-    const deletedSection = await Section.findByIdAndUpdate(sectionId, {isDeleted: true});
-    
-    if(!deletedSection) return res.status(200).json(new ApiResponse(400, {successs: false}, "Section was not deleted"));
-
-    return res.status(200).json(new ApiResponse(200, {success:true}, "Section was deleted successfully"));
-});
-
-const fetchAllSections = asyncHandler(async(req, res) => {
-
-    const {courseId} = req.body;
-
-    if(!courseId) return res.status(200).json(new ApiResponse(400, {success : false}, "Invalid course id"));
+    if (!courseId) return res.status(200).json(new ApiResponse(400, { success: false }, "Invalid Course Id"));
 
     const course = await Course.findById(courseId);
 
-    if(!(course && !course.isDeleted)) return res.status(200).json(new ApiResponse(400, {success: false}, "Course not found"));
+    if (!(course && !course.isDeleted)) return res.status(200).json(new ApiResponse(400, { success: false }, "Course not found"));
 
-    const sections = await Section.find({courseId, isDeleted: false}).select("-isDeleted");
+    if (!sectionName || !sectionDetails) return res.status(200).json(new ApiResponse(400, { success: false }, "Invalid input"));
 
-    if(!sections || sections.length === 0) return res.status(200).json(new ApiResponse(400, {success: false}, "No sections were found"));
+    const sectionExits = await Section.find({ courseId, isDeleted: false, sectionName });
 
-    return res.status(200).json(new ApiResponse(200, {success: true, data : sections}, "All the sections were fetched"));
+    if (sectionExits && sectionExits.length > 0) return res.status(200).json(new ApiResponse(400, { success: false }, 'Section already exists'));
+
+    const section = await Section.create({ courseId, sectionName, sectionDetails });
+
+    if (!section) return res.status(200).json(new ApiResponse(400, { success: false }, "Section was not created"));
+
+    return res.status(200).json(new ApiResponse(200, { success: true, data: section }, "Section was created successfully"));
 });
 
-const fetchSectionContent = asyncHandler(async(req, res) => {});
+const updateSection = asyncHandler(async (req, res) => {
 
-export {addSection, updateSection, deleteSection, fetchAllSections};
+    const { sectionId, ...rest } = req.body;
+
+    if (!sectionId) return res.status(200).json(new ApiResponse(400, { success: false }, 'Section Id is missing'));
+
+    if (!rest) return res.status(200).json(new ApiResponse(400, { success: false }, "Invalid input"));
+
+    const section = await Section.findById(sectionId);
+
+    if (!(section && !section.isDeleted)) return res.status(200).json(new ApiResponse(404, { success: false }, "Section was not found"));
+
+    const updateSection = await Section.findByIdAndUpdate(sectionId, { ...rest }, { new: true });
+
+    if (!updateSection) return res.status(200).json(new ApiResponse(400, { success: false }, "Section was not updated successfully"));
+
+    return res.status(200).json(new ApiResponse(200, { success: true, data: section }, "Section was updated successfully"));
+});
+
+const deleteSection = asyncHandler(async (req, res) => {
+
+    const { sectionId } = req.body;
+
+    if (!sectionId) return res.status(200).json(new ApiResponse(400, { success: false }))
+
+    const section = await Section.findById(sectionId);
+
+    if (!(section && !section.isDeleted)) return res.status(200).json(new ApiResponse(404, { success: false }, "Section was not found"));
+
+    const deletedSection = await Section.findByIdAndUpdate(sectionId, { isDeleted: true });
+
+    if (!deletedSection) return res.status(200).json(new ApiResponse(400, { successs: false }, "Section was not deleted"));
+
+    return res.status(200).json(new ApiResponse(200, { success: true }, "Section was deleted successfully"));
+});
+
+const fetchAllSectionsDetails = asyncHandler(async (req, res) => {
+
+    const { courseId } = req.body;
+
+    if (!courseId) return res.status(200).json(new ApiResponse(400, { success: false }, "Invalid course id"));
+
+    const course = await Course.findById(courseId);
+
+    if (!(course && !course.isDeleted)) return res.status(200).json(new ApiResponse(400, { success: false }, "Course not found"));
+
+    const sections = await Section.find({ courseId, isDeleted: false }).select("-isDeleted");
+
+    if (!sections || sections.length === 0) return res.status(200).json(new ApiResponse(400, { success: false }, "No sections were found"));
+
+    return res.status(200).json(new ApiResponse(200, { success: true, data: sections }, "All the sections were fetched"));
+});
+
+const fetchSectionContentByID = asyncHandler(async (req, res) => {
+
+    const { id } = req.params;
+
+    if (!id) return res.status(200).json(new ApiResponse(400, { success: false }, 'Section Id is missing'));
+
+    const section = await Section.findById(id);
+
+    if (!(section && !section.isDeleted)) return res.status(200).json(new ApiResponse(404, { success: false }, "Section was not found"));
+
+    const sectionContent = await Section.aggregate([{
+        $match: { _id: new mongoose.Types.ObjectId(id) },
+    }, {
+        $lookup: {
+            from: 'mcqs',
+            localField: '_id',
+            foreignField: 'sectionId',
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $eq: ['$isDeleted', false]
+                        }
+                    }
+                }
+            ],
+            as: 'MCQs'
+        }
+    }]);
+
+    if (!sectionContent || sectionContent.length === 0) return res.status(200).json(new ApiResponse(400, { success: false }, 'Section was empty'));
+
+    return res.status(200).json(new ApiResponse(200, { success: true, data: sectionContent }, 'Section content was fetched successfully'));
+
+});
+
+export { addSection, updateSection, deleteSection, fetchAllSectionsDetails, fetchSectionContentByID };
 
--- src/routes/adminRoutes/section.routes.js
+++ src/routes/adminRoutes/section.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/auth.middleware.js";
-import { addSection, deleteSection, fetchAllSections, updateSection } from "../../controllers/adminControllers/section.controllers.js";
+import { addSection, deleteSection, fetchAllSectionsDetails, fetchSectionContentByID, updateSection } from "../../controllers/adminControllers/section.controllers.js";
 
 const router = Router();
 
@@ -10,6 +10,8 @@ router.route("/update-section").post(verifyJWT, updateSection);
 
 router.route("/remove-section").post(verifyJWT, deleteSection);
 
-router.route("/fetch-all-sections").post(verifyJWT, fetchAllSections);
+router.route("/fetch-all-sections").post(verifyJWT, fetchAllSectionsDetails);
+
+router.route("/fetch-section-content-by-id/:id").get(verifyJWT, fetchSectionContentByID);
 
 export default router;
